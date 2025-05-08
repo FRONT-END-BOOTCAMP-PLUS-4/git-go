01572a8ea1fea74f2dbb31ae16945545a7c4c7d5
okang1014 <okang1014@gmail.com>
2025-05-08 10:25:09 +0900

    Chore: 프로젝트 최초 init

---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  semi: true, // 세미콜론 자동 설정
+  singleQuote: false, // 쌍따옴표로 설정
+  tabWidth: 4, // 탭 간격 2칸 고정
+  trailingComma: "es5", // 객체, 배열 등 끝에 쉼표 추가
+  bracketSpacing: true, // 객체 내의 콜론 뒤 공백 추가
+  arrowParens: "always", // 함수 매개변수에 괄호 유지
+  endOfLine: "auto", // OS 별 줄바꿈 문자 자동 설정
+};
