b6b509e804d9c91a3531a3e8b63cbeb2ab67299b
okang1014 <okang1014@gmail.com>
2025-05-13 12:29:11 +0900

    Feat: 사이드 레이아웃 지정 및 목록 레이아웃 지정

--- app/(member)/commits/page.tsx
+++ app/(member)/commits/page.tsx
@@ -1,63 +1,64 @@
 export default function CommitPage() {
     // TODO: 사이드바와 탭 부분은 공통 컴포넌트로 작성해서 각 페이지마다 넣기.
     return (
-        <div className="layout-padding flex gap-x-6 py-6">
-            <aside>
-                <h2>사이드바</h2>
-                <ul className="flex flex-col gap-y-2">
-                    <li>
-                        <a href="/(member)/commits">커밋</a>
+        <div className="layout-padding bg-bg-primary1 flex gap-x-6 py-6">
+            <aside className="border-border-primary1 h-fit min-w-47 rounded-lg border-1 bg-white">
+                <h2 className="border-border-primary1 border-b p-4">
+                    Repositories
+                </h2>
+                <ul>
+                    <li className="border-border-primary1 border-b px-2 py-2 last:border-b-0">
+                        <a href="/(member)/commits">frontend-app</a>
+                    </li>{" "}
+                    <li className="border-border-primary1 border-b px-2 py-2 shadow-inner shadow-amber-400 last:border-b-0">
+                        <a href="/(member)/commits">backend-app</a>
                     </li>
-                    <li>
-                        <a href="/(member)/pull-requests">풀 리퀘스트</a>
-                    </li>
-                    <li>
-                        <a href="/(member)/memoirs">회고</a>
+                    <li className="border-border-primary1 border-b p-2 last:border-b-0">
+                        <a href="/(member)/commits">api</a>
                     </li>
                 </ul>
+                <section className="border-border-primary1 border-t p-4">
+                    <button>저장소 추가</button>
+                </section>
             </aside>
 
             <div className="w-full">
-                <nav>
-                    <ul className="mb-6 flex gap-x-4 border-1">
+                <nav className="bg-white">
+                    <ul className="mb-6 flex gap-x-4">
                         <li>
                             <a href="/(member)/commits">커밋</a>
                         </li>
                         <li>
-                            <a href="/(member)/pull-requests">풀 리퀘스트</a>
+                            <a href="/(member)/pull-requests">Pull Request</a>
                         </li>
                         <li>
-                            <a href="/(member)/memoirs">회고</a>
+                            <a href="/(member)/memoirs">내 회고록</a>
+                        </li>
+                        <li>
+                            <a href="/(member)/memoirs">통계</a>
                         </li>
                     </ul>
                 </nav>
-                <div className="shadow-primary5 shadow-inner">
-                    <section className="flex items-center justify-between p-4">
+                <div className="border-border-primary1 rounded-lg border-1 bg-white">
+                    <section className="border-border-primary1 flex items-center justify-between border-b p-4">
                         <h2 className="font-bold">최근 활동</h2>
-                        <p className="text-sm">{Date().toLocaleString()}</p>
+                        <p className="text-text-secondary2 text-sm">
+                            {Date().toLocaleString()}
+                        </p>
                     </section>
 
                     <ul>
-                        <li>
+                        <li className="p-4">
                             <a href="/(member)/commits/1">커밋 1</a>
                         </li>
-                        <li>
-                            <a href="/(member)/commits/1">커밋 1</a>
+                        <li className="p-4">
+                            <a href="/(member)/commits/2">커밋 2</a>
                         </li>
-                        <li>
-                            <a href="/(member)/commits/1">커밋 1</a>
+                        <li className="p-4">
+                            <a href="/(member)/commits/3">커밋 3</a>
                         </li>
-                        <li>
-                            <a href="/(member)/commits/1">커밋 1</a>
-                        </li>
-                        <li>
-                            <a href="/(member)/commits/1">커밋 1</a>
-                        </li>
-                        <li>
-                            <a href="/(member)/commits/1">커밋 1</a>
-                        </li>
-                        <li>
-                            <a href="/(member)/commits/1">커밋 1</a>
+                        <li className="p-4">
+                            <a href="/(member)/commits/4">커밋 4</a>
                         </li>
                     </ul>
                 </div>
