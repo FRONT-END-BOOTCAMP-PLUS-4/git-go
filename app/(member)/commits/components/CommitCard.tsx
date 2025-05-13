9a02ada8c63acacdbeb78a212071d785959c6004
okang1014 <okang1014@gmail.com>
2025-05-13 17:44:38 +0900

    Feat: member/commits 커밋 목록 페이지 UI 작업 완료 (#30)
    * Chore: 공통 스타일 추가
    
    * Chore: 불필요한 아이콘 삭제
    
    * Design: global.css 기본 레이아웃 지정
    
    * Chore: 폴더명 수정
    
    * Style: text-secondary 추가
    
    * Feat: 사이드 레이아웃 지정 및 목록 레이아웃 지정
    
    * Feat: app > layout.tsx 배경색 지정
    
    * Feat: commits > page.tsx 커밋 목록 디자인 작업
    
    * Feat: commits/components > CommitCard.tsx 컴포넌트 분리
    
    Feat: commits/components > CommitCard.tsx 컴포넌트 분리
    
    Feat: commits/components > CommitCard.tsx 컴포넌트 분리
    
    Feat: commits/components > CommitCard.tsx 컴포넌트 분리
    
    * Feat: app/member > Button.tsx 컴포넌트 적용
    
    * Feat: app/member > 저장소 추가 버튼 Button.tsx 컴포넌트로 수정
    
    * Feat: app/member/commits > 사이드바 스타일링
    
    * Feat: app/member/commits > 사이드바 토글 완료

---
+++ app/(member)/commits/components/CommitCard.tsx
@@ -0,0 +1,91 @@
+import Button from "@/app/components/Button";
+import Image from "next/image";
+
+interface LabelBadgeProps {
+    type: "bugfix" | "feature" | "refactor";
+}
+
+const typeClassMap: Record<
+    LabelBadgeProps["type"],
+    { bg: string; text: string; label: string }
+> = {
+    bugfix: {
+        label: "bugfix",
+        bg: "bg-[#fee2e2]",
+        text: "text-[#991b1b]",
+    },
+    feature: {
+        label: "feature",
+        bg: "bg-[#d1fae5]",
+        text: "text-[#065f46]",
+    },
+    refactor: {
+        label: "refactor",
+        bg: "bg-[#e0f2fe]",
+        text: "text-[#1e40af]",
+    },
+};
+
+export default function CommitCard({ type }: LabelBadgeProps) {
+    const { label, bg, text } = typeClassMap[type];
+
+    return (
+        <li className="border-border-primary1 border-b p-4 last:border-b-0">
+            <article className="flex items-start gap-x-4">
+                <div className="bg-primary2 flex h-10 w-10 items-center justify-center rounded-full">
+                    <Image
+                        src="commit-blue.svg"
+                        width={20}
+                        height={16}
+                        alt="커밋 아이콘"
+                    />
+                </div>
+                <div className="flex flex-1 flex-col gap-y-1">
+                    <div className="text-text-secondary2 flex items-center gap-x-3 text-xs">
+                        <p>bd2a0c2</p>
+                        <div
+                            className={`shadow-border-primary1 rounded-lg px-3 py-1 font-semibold ${bg} ${text} shadow-sm`}
+                        >
+                            {label}
+                        </div>
+                        <p className="ml-auto">2 hours ago</p>
+                    </div>
+                    <h3 className="font-semibold">
+                        Fix navigation bug in dashboard component
+                    </h3>
+                    <div className="flex items-center gap-x-3">
+                        <div className="flex items-center gap-x-1">
+                            <Image
+                                src="branch.svg"
+                                alt="브랜치 아이콘"
+                                width={14}
+                                height={12}
+                            />
+                            <p>frontend-app</p>
+                        </div>
+                        <div className="flex items-center gap-x-1">
+                            <Image
+                                src="code.svg"
+                                alt="코드 아이콘"
+                                width={18}
+                                height={14}
+                            />
+                            bugfix/nav-issue
+                        </div>
+                        <div className="ml-auto">
+                            <Button type="lined" htmlType="button">
+                                <Image
+                                    src="write.svg"
+                                    alt="회고 등록 아이콘"
+                                    width={12}
+                                    height={12}
+                                />
+                                Write Memoir
+                            </Button>
+                        </div>
+                    </div>
+                </div>
+            </article>
+        </li>
+    );
+}
--- app/(member)/commits/page.tsx
+++ app/(member)/commits/page.tsx
@@ -1,26 +1,127 @@
+"use client";
+
+import CommitCard from "@/app/(member)/commits/components/CommitCard";
+import RepoSelectModal from "@/app/(member)/components/RepoSelectModal";
+import Button from "@/app/components/Button";
+import Image from "next/image";
+import { useState } from "react";
+
 export default function CommitPage() {
     // TODO: 사이드바와 탭 부분은 공통 컴포넌트로 작성해서 각 페이지마다 넣기.
-    return <div>CommitPage</div>;
+
+    const date = new Date().toLocaleString();
+    const onlyDate = date.split(", ")[0];
+    const dateObj = new Date(onlyDate);
+
+    // 현재 날짜를 한국어 형식으로 포맷팅
+    const formattedDate = new Intl.DateTimeFormat("ko-KR", {
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+    }).format(dateObj);
+
+    const [open, setOpen] = useState(false);
+
+    const branches = [
+        { name: "frontend-app", icon: "branch-blue.svg" },
+        { name: "backend-app", icon: "branch.svg" },
+        { name: "api", icon: "branch.svg" },
+    ];
+
+    const [selectedBranch, setSelectedBranch] = useState("frontend-app");
+
+    return (
+        <div className="layout-padding bg-bg-primary1 flex gap-x-6 py-6">
+            <RepoSelectModal open={open} onClose={() => setOpen(false)} />
+            <aside className="border-border-primary1 h-fit min-w-47 rounded-lg border-1 bg-white">
+                <h2 className="border-border-primary1 border-b p-4 font-semibold">
+                    Repositories
+                </h2>
+                <ul className="flex flex-col gap-y-1 p-2">
+                    {branches.map((branch) => {
+                        const isSelected = branch.name === selectedBranch;
+                        return (
+                            <li
+                                key={branch.name}
+                                className="border-border-primary1"
+                            >
+                                <button
+                                    className={`flex w-full items-center gap-x-2 rounded-md p-2 font-semibold ${
+                                        isSelected
+                                            ? "bg-primary2 text-primary7"
+                                            : ""
+                                    }`}
+                                    onClick={() =>
+                                        setSelectedBranch(branch.name)
+                                    }
+                                >
+                                    <Image
+                                        src={
+                                            isSelected
+                                                ? "branch-blue.svg"
+                                                : "branch.svg"
+                                        }
+                                        width={14}
+                                        height={14}
+                                        alt="브랜치 아이콘"
+                                    />
+                                    {branch.name}
+                                </button>
+                            </li>
+                        );
+                    })}
+                </ul>
+                <section className="border-border-primary1 border-t p-3">
+                    <Button
+                        type="lined"
+                        size="full"
+                        onClick={() => setOpen(true)}
+                    >
+                        <Image
+                            src="plus-gray.svg"
+                            width={10.5}
+                            height={16}
+                            alt="저장소 추가 아이콘"
+                        />
+                        저장소 추가
+                    </Button>
+                </section>
+            </aside>
+
+            <div className="w-full">
+                <nav className="bg-white">
+                    <ul className="mb-6 flex gap-x-4">
+                        <li>
+                            <a href="/(member)/commits">커밋</a>
+                        </li>
+                        <li>
+                            <a href="/(member)/pull-requests">Pull Request</a>
+                        </li>
+                        <li>
+                            <a href="/(member)/memoirs">내 회고록</a>
+                        </li>
+                        <li>
+                            <a href="/(member)/memoirs">통계</a>
+                        </li>
+                    </ul>
+                </nav>
+                <div className="border-border-primary1 rounded-lg border-1 bg-white">
+                    <section className="border-border-primary1 flex items-center justify-between border-b p-4">
+                        <h2 className="font-bold">최근 활동</h2>
+                        <p className="text-text-secondary2 text-sm">
+                            {formattedDate}
+                        </p>
+                    </section>
+
+                    <ul>
+                        {/* CommitCard 의 props 로 커밋의 타입을 지정 - bugfix | feature | refactor */}
+                        <CommitCard type="bugfix" />
+                        <CommitCard type="feature" />
+                        <CommitCard type="refactor" />
+                        <CommitCard type="bugfix" />
+                    </ul>
+                </div>
+            </div>
+        </div>
+    );
 }
-
-// "use client";
-
-// import { useState } from "react";
-// import RepoSelectModal from "../components/RepoSelectModal";
-
-// export default function CommitPage() {
-//     const [open, setOpen] = useState(false);
-
-//     return (
-//         <div className="p-4">
-//             <button
-//                 onClick={() => setOpen(true)}
-//                 className="bg-blue-600 text-white px-4 py-2 rounded-md"
-//             >
-//                 저장소 추가
-//             </button>
-
-//             <RepoSelectModal open={open} onClose={() => setOpen(false)} />
-//         </div>
-//     );
-// }
