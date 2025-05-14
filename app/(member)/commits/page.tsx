7998cb64145fe35633d95005f3a0a4a7a26dc107
Parkchanwoo <parkchanwoo7@naver.com>
2025-05-14 13:52:44 +0900

    Feat: 커밋페이지에서 로그인한 유저가 연동한 레포가 없으면 저장소추가모달 뜨도록 구현

--- app/(member)/commits/page.tsx
+++ app/(member)/commits/page.tsx
@@ -1,7 +1,8 @@
 "use client";
 
 import CommitCard from "@/app/(member)/commits/components/CommitCard";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import RepoSelectModal from "../components/RepoSelectModal";
 
 export default function CommitPage() {
     // TODO: 사이드바와 탭 부분은 공통 컴포넌트로 작성해서 각 페이지마다 넣기.
@@ -15,6 +16,22 @@ export default function CommitPage() {
         day: "numeric",
     }).format(now);
 
+    useEffect(() => {
+        const fetchUserRepos = async () => {
+            try {
+                const res = await fetch("/api/repos/user");
+                const repos = await res.json();
+                if (Array.isArray(repos) && repos.length === 0) {
+                    setOpen(true);
+                }
+            } catch (err) {
+                console.error("유저 저장소 확인 실패", err);
+            }
+        };
+
+        fetchUserRepos();
+    });
+
     const [open, setOpen] = useState(false);
 
     const branches = [
@@ -24,19 +41,22 @@ export default function CommitPage() {
     ];
 
     return (
-        <div className="border-border-primary1 rounded-lg border-1 bg-white">
-            <section className="border-border-primary1 flex items-center justify-between border-b p-4">
-                <h2 className="font-bold">최근 활동</h2>
-                <p className="text-text-secondary2 text-sm">{formattedDate}</p>
-            </section>
+        <>
+            <RepoSelectModal open={open} onClose={() => setOpen(false)} />
+            <div className="border-border-primary1 rounded-lg border-1 bg-white">
+                <section className="border-border-primary1 flex items-center justify-between border-b p-4">
+                    <h2 className="font-bold">최근 활동</h2>
+                    <p className="text-text-secondary2 text-sm">{formattedDate}</p>
+                </section>
 
-            <ul>
-                {/* CommitCard 의 props 로 커밋의 타입을 지정 - bugfix | feature | refactor */}
-                <CommitCard type="bugfix" />
-                <CommitCard type="feature" />
-                <CommitCard type="refactor" />
-                <CommitCard type="bugfix" />
-            </ul>
-        </div>
+                <ul>
+                    {/* CommitCard 의 props 로 커밋의 타입을 지정 - bugfix | feature | refactor */}
+                    <CommitCard type="bugfix" />
+                    <CommitCard type="feature" />
+                    <CommitCard type="refactor" />
+                    <CommitCard type="bugfix" />
+                </ul>
+            </div>
+        </>
     );
 }
