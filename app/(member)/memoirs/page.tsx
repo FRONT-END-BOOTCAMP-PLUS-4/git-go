f3ba35ba019f295b8059b64e488fc339d34c8f39
okang1014 <okang1014@gmail.com>
2025-05-14 17:48:12 +0900

    Feat: (member)/memoir > 회고 목록 페이지 UI 작업 완료

--- app/(member)/memoirs/page.tsx
+++ app/(member)/memoirs/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import CommitCard from "@/app/(member)/commits/components/CommitCard";
+import MemoirCard from "@/app/(member)/memoirs/components/MemoirCard";
 
 export default function MemoirPage() {
     // TODO: 사이드바와 탭 부분은 공통 컴포넌트로 작성해서 각 페이지마다 넣기.
@@ -17,16 +17,13 @@ export default function MemoirPage() {
     return (
         <div className="border-border-primary1 rounded-lg border-1 bg-white">
             <section className="border-border-primary1 flex items-center justify-between border-b p-4">
-                <h2 className="font-bold">최근 활동</h2>
+                <h2 className="font-bold">My Memoirs</h2>
                 <p className="text-text-secondary2 text-sm">{formattedDate}</p>
             </section>
 
             <ul>
-                {/* CommitCard 의 props 로 커밋의 타입을 지정 - bugfix | feature | refactor */}
-                <CommitCard type="bugfix" />
-                <CommitCard type="feature" />
-                <CommitCard type="refactor" />
-                <CommitCard type="bugfix" />
+                <MemoirCard type="commit" />
+                <MemoirCard type="pr" />
             </ul>
         </div>
     );
