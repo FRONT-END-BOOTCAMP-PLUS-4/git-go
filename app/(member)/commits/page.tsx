ff9d7aa4d240806238c71acd16ac21c51b89bfa1
okang1014 <okang1014@gmail.com>
2025-05-14 10:10:04 +0900

    Refactor: 사이드바 컴포넌트 분리 및 member/layout.tsx 추가

--- app/(member)/commits/page.tsx
+++ app/(member)/commits/page.tsx
@@ -27,85 +27,20 @@ export default function CommitPage() {
         { name: "api", icon: "branch.svg" },
     ];
 
-    const [selectedBranch, setSelectedBranch] = useState("frontend-app");
-
     return (
-        <div className="layout-padding bg-bg-primary1 flex gap-x-6 py-6">
-            <RepoSelectModal open={open} onClose={() => setOpen(false)} />
-            <aside className="border-border-primary1 h-fit min-w-47 rounded-lg border-1 bg-white">
-                <h2 className="border-border-primary1 border-b p-4 font-semibold">
-                    Repositories
-                </h2>
-                <ul className="flex flex-col gap-y-1 p-2">
-                    {branches.map((branch) => {
-                        const isSelected = branch.name === selectedBranch;
-                        return (
-                            <li
-                                key={branch.name}
-                                className="border-border-primary1"
-                            >
-                                <button
-                                    className={`flex w-full items-center gap-x-2 rounded-md p-2 font-semibold ${
-                                        isSelected
-                                            ? "bg-primary2 text-primary7"
-                                            : ""
-                                    }`}
-                                    onClick={() =>
-                                        setSelectedBranch(branch.name)
-                                    }
-                                >
-                                    <Image
-                                        src={
-                                            isSelected
-                                                ? "branch-blue.svg"
-                                                : "branch.svg"
-                                        }
-                                        width={14}
-                                        height={14}
-                                        alt="브랜치 아이콘"
-                                    />
-                                    {branch.name}
-                                </button>
-                            </li>
-                        );
-                    })}
-                </ul>
-                <section className="border-border-primary1 border-t p-3">
-                    <Button
-                        type="lined"
-                        size="full"
-                        onClick={() => setOpen(true)}
-                    >
-                        <Image
-                            src="plus-gray.svg"
-                            width={10.5}
-                            height={16}
-                            alt="저장소 추가 아이콘"
-                        />
-                        저장소 추가
-                    </Button>
-                </section>
-            </aside>
+        <div className="border-border-primary1 rounded-lg border-1 bg-white">
+            <section className="border-border-primary1 flex items-center justify-between border-b p-4">
+                <h2 className="font-bold">최근 활동</h2>
+                <p className="text-text-secondary2 text-sm">{formattedDate}</p>
+            </section>
 
-            <div className="w-full">
-                <PageTap status="commit" />
-                <div className="border-border-primary1 mt-6 rounded-lg border-1 bg-white">
-                    <section className="border-border-primary1 flex items-center justify-between border-b p-4">
-                        <h2 className="font-bold">최근 활동</h2>
-                        <p className="text-text-secondary2 text-sm">
-                            {formattedDate}
-                        </p>
-                    </section>
-
-                    <ul>
-                        {/* CommitCard 의 props 로 커밋의 타입을 지정 - bugfix | feature | refactor */}
-                        <CommitCard type="bugfix" />
-                        <CommitCard type="feature" />
-                        <CommitCard type="refactor" />
-                        <CommitCard type="bugfix" />
-                    </ul>
-                </div>
-            </div>
+            <ul>
+                {/* CommitCard 의 props 로 커밋의 타입을 지정 - bugfix | feature | refactor */}
+                <CommitCard type="bugfix" />
+                <CommitCard type="feature" />
+                <CommitCard type="refactor" />
+                <CommitCard type="bugfix" />
+            </ul>
         </div>
     );
 }
