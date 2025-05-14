978e3adbc15360b359bed23fb25a6a768035c300
Parkchanwoo <parkchanwoo7@naver.com>
2025-05-14 15:00:23 +0900

    Feat: 로그아웃 기능 추가

---
+++ app/components/LogoutButton.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import { signOut } from "next-auth/react";
+import Button from "./Button";
+
+export default function LogoutButton() {
+    return (
+        <Button type="lined" htmlType="button" onClick={() => signOut({ callbackUrl: "/" })}>
+            로그아웃
+        </Button>
+    );
+}
