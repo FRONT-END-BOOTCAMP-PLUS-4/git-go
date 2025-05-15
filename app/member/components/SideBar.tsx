b49a6580ddca63f99be52ca3c31985625fc74996
okang1014 <okang1014@gmail.com>
2025-05-15 10:36:51 +0900

    Feat: member/components > SideBar.tsx hover 시, 선택 시 디자인 변경 추가

--- app/member/components/SideBar.tsx
+++ app/member/components/SideBar.tsx
@@ -52,11 +52,11 @@ export default function SideBar({
 
     return (
         <aside className="flex flex-col gap-y-4">
-            <div className="border-border-primary1 h-fit min-w-47 rounded-lg border-1 bg-white">
+            <div className="border-border-primary1 h-fit w-50 rounded-lg border-1 bg-white">
                 <h2 className="border-border-primary1 border-b p-4 font-semibold">
                     Repositories
                 </h2>
-                <ul className="flex flex-col gap-y-1 p-2">
+                <ul className="flex w-full flex-col gap-y-1 p-2">
                     {userRepos.map((repo) => {
                         const isSelected = repo.nameWithOwner === selectedRepo;
                         return (
@@ -65,7 +65,7 @@ export default function SideBar({
                                 className="border-border-primary1"
                             >
                                 <button
-                                    className={`flex w-full items-center gap-x-2 rounded-md px-2 py-2 text-left font-semibold ${
+                                    className={`flex w-full cursor-pointer items-center gap-x-2 rounded-md px-2 py-2 text-left font-semibold ${
                                         isSelected
                                             ? "bg-primary2 text-primary7"
                                             : ""
@@ -84,7 +84,9 @@ export default function SideBar({
                                         height={14}
                                         alt="브랜치 아이콘"
                                     />
-                                    <span className="text-sm">
+                                    <span
+                                        className={`text-sm hover:line-clamp-none ${selectedRepo === repo.nameWithOwner ? "line-clamp-none" : "line-clamp-2"}`}
+                                    >
                                         {repo.nameWithOwner}
                                     </span>
                                 </button>
@@ -123,97 +125,3 @@ export default function SideBar({
         </aside>
     );
 }
-
-// "use client";
-// import Button from "@/app/components/Button";
-// import Image from "next/image";
-// import { useState } from "react";
-// import CommitPrFilter from "../memoirs/components/Filter/CommitPrFilter";
-// import { useParams, usePathname } from "next/navigation";
-// import TimeFilter from "./TimeFilter";
-
-// export default function SideBar({
-//     setOpen,
-// }: {
-//     setOpen: (open: boolean) => void;
-// }) {
-//     const [selectedBranch, setSelectedBranch] = useState("frontend-app");
-
-//     const branches = [
-//         { name: "frontend-app", icon: "branch-blue.svg" },
-//         { name: "backend-app", icon: "branch.svg" },
-//         { name: "api", icon: "branch.svg" },
-//     ];
-
-//     const pathname = usePathname();
-//     console.log(pathname);
-
-//     return (
-//         <aside className="flex flex-col gap-y-4">
-//             <div className="border-border-primary1 h-fit min-w-47 rounded-lg border-1 bg-white">
-//                 <h2 className="border-border-primary1 border-b p-4 font-semibold">
-//                     Repositories
-//                 </h2>
-//                 <ul className="flex flex-col gap-y-1 p-2">
-//                     {branches.map((branch) => {
-//                         const isSelected = branch.name === selectedBranch;
-//                         return (
-//                             <li
-//                                 key={branch.name}
-//                                 className="border-border-primary1"
-//                             >
-//                                 <button
-//                                     className={`flex w-full items-center gap-x-2 rounded-md p-2 font-semibold ${
-//                                         isSelected
-//                                             ? "bg-primary2 text-primary7"
-//                                             : ""
-//                                     }`}
-//                                     onClick={() =>
-//                                         setSelectedBranch(branch.name)
-//                                     }
-//                                 >
-//                                     <Image
-//                                         src={
-//                                             isSelected
-//                                                 ? "branch-blue.svg"
-//                                                 : "branch.svg"
-//                                         }
-//                                         width={14}
-//                                         height={14}
-//                                         alt="브랜치 아이콘"
-//                                     />
-//                                     {branch.name}
-//                                 </button>
-//                             </li>
-//                         );
-//                     })}
-//                 </ul>
-//                 <section className="border-border-primary1 border-t p-3">
-//                     <Button
-//                         type="lined"
-//                         size="full"
-//                         onClick={() => setOpen(true)}
-//                     >
-//                         <Image
-//                             src="plus-gray.svg"
-//                             width={10.5}
-//                             height={16}
-//                             alt="저장소 추가 아이콘"
-//                         />
-//                         저장소 추가
-//                     </Button>
-//                 </section>
-//             </div>
-//             {(pathname.includes("memoirs") || pathname.includes("stats")) && (
-//                 <TimeFilter
-//                     options={[
-//                         { value: "7days", label: "Last 7 days" },
-//                         { value: "30days", label: "Last 30 days" },
-//                         { value: "90days", label: "Last 90 days" },
-//                     ]}
-//                 />
-//             )}
-//             {pathname.includes("memoirs") && <CommitPrFilter />}
-//         </aside>
-//     );
-// }
