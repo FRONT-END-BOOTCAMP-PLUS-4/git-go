7aedfb4c73d151769f6b85043e372766bcf9326e
Parkchanwoo <parkchanwoo7@naver.com>
2025-05-15 10:54:10 +0900

    Chore: GihubRepoRepository.ts 파일들에서 dto없애고 엔티티로 하도록 수정

---
+++ domain/entities/GitHubRepo.ts
@@ -0,0 +1,14 @@
+export class GitHubRepo {
+    constructor(
+        public readonly id: string,
+        public readonly name: string,
+        public readonly nameWithOwner: string,
+        public readonly url: string,
+        public readonly isPrivate: boolean,
+        public readonly description: string | null,
+        public readonly updatedAt: Date,
+        public readonly stargazerCount: number,
+        public readonly languageName?: string,
+        public readonly languageColor?: string
+    ) { }
+}
