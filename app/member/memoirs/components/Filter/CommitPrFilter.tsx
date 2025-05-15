4589f5129abc82d0f488cf32ebd789de6b511368
dongsuseo <poopoo0515@naver.com>
2025-05-15 12:04:21 +0900

    Feat: 태그 필터 추가

--- app/member/memoirs/components/Filter/CommitPrFilter.tsx
+++ app/member/memoirs/components/Filter/CommitPrFilter.tsx
@@ -7,7 +7,7 @@ export default function CommitPrFilter() {
     const { filters, toggleFilter } = useFilterStore();
 
     return (
-        <div className="border-border-primary1 shadow-sms rounded-lg border-1 bg-white p-4">
+        <div>
             <div className="mb-2 text-base font-normal">필터</div>
             <div className="space-y-2 text-sm">
                 <div className="flex items-center space-x-2">
