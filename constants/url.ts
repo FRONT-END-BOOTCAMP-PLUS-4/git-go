862857efb188bcf1073fcf7449c10a53115cf552
okang1014 <okang1014@gmail.com>
2025-05-15 10:04:46 +0900

    Chore: member 관련 URL 상수 추가

--- constants/url.ts
+++ constants/url.ts
@@ -1,6 +1,7 @@
 export const MEMBER_URL = {
   main: "/member",
   commits: "/member/commits",
-  memoirs: "/member/memoirs",
   prs: "/member/pull-requests",
+  memoirs: "/member/memoirs",
+  stats: "/member/stats",
 };
