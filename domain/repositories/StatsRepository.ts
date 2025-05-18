e0b5b6f46f65ad56b5bde108a94ccaf777b3310e
Parkchanwoo <parkchanwoo7@naver.com>
2025-05-18 23:21:03 +0900

    Feat: 통계페이지에서 레포별로 코드라인 수 일주일전과 비교해서 %로 보여주는 기능 구현

--- domain/repositories/StatsRepository.ts
+++ domain/repositories/StatsRepository.ts
@@ -1,6 +1,6 @@
 export interface StatsRepository {
     fetchStats(repo: string): Promise<{ totalCommits: number }>;
-    fetchLines(repo: string): Promise<{ totalLines: number }>;
+    fetchLines(repo: string): Promise<{ totalLines: number; prevLines: number }>;
     resolveNameWithOwner(repoId: string): Promise<string>;
     fetchWeeklyCommits(repo: string): Promise<{ date: string; count: number }[]>;
 }
