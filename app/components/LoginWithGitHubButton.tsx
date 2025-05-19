3395157296b289af1dfbbe29a28fa33b32c6a84f
dongsuseo <poopoo0515@naver.com>
2025-05-19 17:38:43 +0900

    Style: 헤더 UI 수정

--- app/components/LoginWithGitHubButton.tsx
+++ app/components/LoginWithGitHubButton.tsx
@@ -7,7 +7,7 @@ import { MEMBER_URL } from "@/constants/url";
 
 export default function LoginWithGitHubButton() {
     return (
-        <div className="flex justify-center">
+        <div className="flex justify-center px-0">
             <Button
                 type="default"
                 size="regular"
