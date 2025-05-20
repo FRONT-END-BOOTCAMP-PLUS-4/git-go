19de9b5583eef296bfc5c9fdbc466bc7a25aec4a
dongsuseo <poopoo0515@naver.com>
2025-05-20 11:31:01 +0900

    Chore: ai prompt를 상수로 분리

--- app/member/components/CreateMemoir/AiSummary.tsx
+++ app/member/components/CreateMemoir/AiSummary.tsx
@@ -6,6 +6,7 @@ import { GoogleGenAI } from "@google/genai";
 import { flushSync } from "react-dom";
 import { useSimplifyCommitData } from "@/hooks/useSimplifyCommitData"; // 정제 함수 사용
 import { COMMITS } from "@/constants/mockCommits"; // 커밋 예시 데이터
+import { PROMPT } from "@/constants/aiPrompt"; // AI 프롬프트 상수
 import ReactMarkdown from "react-markdown";
 
 const ai = new GoogleGenAI({ apiKey: process.env.NEXT_PUBLIC_GEMINI_API_KEY });
@@ -22,30 +23,11 @@ export default function AiSummary() {
         const simplified = useSimplifyCommitData(COMMITS);
 
         const prompt = `
-다음은 Git 변경 내역입니다. 이 변경 내역을 아래 출력 형식에 맞게 정리해주세요.
-
-- 각 변경된 파일의 내용을 요약해주세요.
-- 기술적인 설명은 간단하고 명확하게 적어주세요.
-- 각 헤더(요약, 파일이름) 다음에는 한 줄 띄운 후 내용을 작성하세요.
-- 너무 긴 문장은 **적절히 끊어서** 가독성을 높여주세요.
-- 각 섹션(요약, 파일1, 파일2, …) 사이에도 빈 줄 1개를 추가하세요.
-- 출력은 반드시 아래 형식을 따르세요.
-
-
-형식 예시:
-
-**요약**
-변경된 전체 내용을 간단히 요약
-
-**파일이름1**
-해당 파일에서 변경된 주요 사항 설명
-
-**파일이름2**
-해당 파일에서 변경된 주요 사항 설명
-\`\`\`json
-${JSON.stringify(simplified, null, 2)}
-\`\`\`
-`;
+                        ${PROMPT}
+                        \`\`\`json
+                        ${JSON.stringify(simplified, null, 2)}
+                        \`\`\`
+                        `;
 
         const response = await ai.models.generateContentStream({
             model: "gemini-2.5-flash-preview-04-17",
