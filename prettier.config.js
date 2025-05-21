955c289abb88fb7972d2ef7a317f0d9b71372cdb
okang1014 <okang1014@gmail.com>
2025-05-21 09:45:49 +0900

    Chore: Prettier 설정 파일 확장자명 변경

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+export default {
+  "semi": true,
+  "singleQuote": false,
+  "tabWidth": 4,
+  "trailingComma": "es5",
+  "bracketSpacing": true,
+  "arrowParens": "always",
+  "endOfLine": "auto",
+  "printWidth": 80,
+  "plugins": ["prettier-plugin-tailwindcss"]
+};
