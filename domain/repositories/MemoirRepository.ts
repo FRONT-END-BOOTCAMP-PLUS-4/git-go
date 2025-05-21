86e21c5974da4a12555e8ef53c9f0c84d8cb621f
Yoonstar <yagobo1110@naver.com>
2025-05-21 09:46:44 +0900

    Feat: 회고록 작성 백엔드 아키텍처 생성

--- domain/repositories/MemoirRepository.ts
+++ domain/repositories/MemoirRepository.ts
@@ -1,3 +1,16 @@
+import { Memoir } from "@/prisma/generated/prisma";
+
 export interface MemoirRepository {
     countByRepoName(name: string): Promise<number>;
+
+    create(data: {
+        title: string;
+        content: string;
+        source: string;
+        userId: string;
+        repoId: number;
+        typeId: number;
+        aiSum?: string;
+        tags?: string[];
+    }): Promise<Memoir>;
 }
