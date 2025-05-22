f217f0bb7f84c355e91d918c3a18191efe74793a
dongsuseo <poopoo0515@naver.com>
2025-05-22 10:15:59 +0900

    Feat: 검색창 우측 상단으로 배치

--- app/member/memoirs/components/Filter/SearchFilter.tsx
+++ app/member/memoirs/components/Filter/SearchFilter.tsx
@@ -4,15 +4,13 @@ import { Search } from "lucide-react";
 
 export default function SearchFilter() {
     return (
-        <div className="border-border-primary1 mt-4 rounded-lg border bg-white p-4">
-            <div className="border-border-primary1 flex items-center rounded-lg border px-3 py-2">
-                <Search className="text-text-secondary2" />
-                <input
-                    className="text-text-secondary2 placeholder-text-secondary2 ml-2 w-full outline-none"
-                    type="text"
-                    placeholder="회고록 검색..."
-                />
-            </div>
+        <div className="border-border-primary1 ml-4 flex h-9 items-center rounded-lg border bg-white px-3 py-2">
+            <Search className="text-text-secondary2" />
+            <input
+                className="text-text-secondary1 placeholder-text-secondary2 ml-2 w-full text-sm outline-none"
+                type="text"
+                placeholder="회고록 검색..."
+            />
         </div>
     );
 }
