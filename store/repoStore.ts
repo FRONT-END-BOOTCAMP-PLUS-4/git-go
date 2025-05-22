e854dbfbbd4e0fe67c020eb360f3e11a738d2f9a
Parkchanwoo <parkchanwoo7@naver.com>
2025-05-22 11:54:35 +0900

    Feat: repoStore에 dbid도 추가하고 값 나오도록 구현

--- store/repoStore.ts
+++ store/repoStore.ts
@@ -1,12 +1,14 @@
 import { create } from "zustand";
+import { createJSONStorage, persist } from "zustand/middleware";
 
 // 1. Zustand 스토어 타입 정의
 type RepoStore = {
   selectedRepo: {
+    dbid: number;
     id: string;
     nameWithOwner: string;
   } | null;
-  setSelectedRepo: (repo: { id: string; nameWithOwner: string; } | null) => void;
+  setSelectedRepo: (repo: { dbid: number; id: string; nameWithOwner: string; } | null) => void;
   // selectedRepo: string | null;
   // setSelectedRepo: (nameWithOwner: string | null) => void;
   reloadRepoList: boolean;
@@ -15,10 +17,15 @@ type RepoStore = {
 };
 
 // 2. Zustand 스토어 생성
-export const useRepoStore = create<RepoStore>((set) => ({
-  selectedRepo: null,
-  setSelectedRepo: (repo) => set({ selectedRepo: repo }),
-  reloadRepoList: false,
-  triggerReload: () => set({ reloadRepoList: true }),
-  resetReload: () => set({ reloadRepoList: false }),
-}));
+export const useRepoStore = create<RepoStore>()(
+  persist<RepoStore>(
+    (set) => ({
+      selectedRepo: null,
+      setSelectedRepo: (repo) => set({ selectedRepo: repo }),
+      reloadRepoList: false,
+      triggerReload: () => set({ reloadRepoList: true }),
+      resetReload: () => set({ reloadRepoList: false }),
+    }),
+    { name: "repo-store", storage: createJSONStorage(() => sessionStorage), }
+  )
+);
