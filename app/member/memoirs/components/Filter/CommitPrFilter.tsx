35fd1082df6fee7977bf1b42c62a9284bd8a762b
Parkchanwoo <parkchanwoo7@naver.com>
2025-05-23 10:05:05 +0900

    Chore: 회고목록페이지에서 전체보기,커밋,pr 필터 글씨 눌러도 동작하게 수정

--- app/member/memoirs/components/Filter/CommitPrFilter.tsx
+++ app/member/memoirs/components/Filter/CommitPrFilter.tsx
@@ -10,23 +10,29 @@ export default function CommitPrFilter() {
         <div>
             <div className="mb-2 text-base font-normal">필터</div>
             <div className="space-y-2 text-sm">
-                {["all", "commits", "pullRequests"].map((type) => (
-                    <div key={type} className="flex items-center space-x-2">
-                        <input
-                            type="radio"
-                            className="form-radio cursor-pointer"
-                            checked={filterType === type}
-                            onChange={() => setFilterType(type as any)}
-                        />
-                        <div>
-                            {type === "all"
-                                ? "전체보기"
-                                : type === "commits"
-                                    ? "Commits"
-                                    : "Pull Requests"}
+                {["all", "commits", "pullRequests"].map((type) => {
+                    const id = `filter-${type}`;
+                    return (
+                        <div key={type} className="flex items-center space-x-2">
+                            <input
+                                type="radio"
+                                id={id}
+                                className="form-radio cursor-pointer"
+                                name="filterType"
+                                value={type}
+                                checked={filterType === type}
+                                onChange={() => setFilterType(type as any)}
+                            />
+                            <label htmlFor={id} className="cursor-pointer">
+                                {type === "all"
+                                    ? "전체보기"
+                                    : type === "commits"
+                                        ? "Commits"
+                                        : "Pull Requests"}
+                            </label>
                         </div>
-                    </div>
-                ))}
+                    );
+                })}
             </div>
         </div>
     );
