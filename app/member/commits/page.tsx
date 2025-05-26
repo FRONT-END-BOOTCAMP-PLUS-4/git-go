e48a3bc60af8904ca6132b8f7aa48344ded5e2a5
okang1014 <okang1014@gmail.com>
2025-05-26 22:14:26 +0900

    Chore: 불필요한 import 삭제

--- app/member/commits/page.tsx
+++ app/member/commits/page.tsx
@@ -7,7 +7,6 @@ import { useRepoStore } from "@/store/repoStore";
 import { useSession } from "next-auth/react";
 import Pagination from "@/app/components/Pagination";
 import { CommitCardSkeleton } from "@/app/member/commits/components/CommitCardSkeleton";
-import Image from "next/image";
 import EmptyResult from "@/app/member/components/EmptyResult";
 
 interface Commit {
