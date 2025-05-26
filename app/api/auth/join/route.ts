93e9f85e977a299198e1b321c4773157991f8b39
Parkchanwoo <parkchanwoo7@naver.com>
2025-05-26 22:21:32 +0900

    Chore: joinUser, WithdrawUser 파일명 뒤에 Usecase붙이기

--- app/api/auth/join/route.ts
+++ app/api/auth/join/route.ts
@@ -1,12 +1,12 @@
 import { NextRequest, NextResponse } from "next/server";
 import { PrUserRepository } from "@/infra/repositories/prisma/PrUserRepository";
-import { JoinUser } from "@/application/usecase/user/JoinUser";
+import { JoinUserUsecase } from "@/application/usecase/user/JoinUserUsecase";
 
 export async function POST(req: NextRequest) {
     const { githubId, username, profileUrl } = await req.json();
 
     const repo = new PrUserRepository();
-    const usecase = new JoinUser(repo);
+    const usecase = new JoinUserUsecase(repo);
     const user = await usecase.execute({ githubId, username, profileUrl });
 
     return NextResponse.json(user);
