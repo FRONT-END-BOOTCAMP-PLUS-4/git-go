a17fa8b11a603208f9db3e05d2e78446e6353d25
Parkchanwoo <parkchanwoo7@naver.com>
2025-05-26 10:28:29 +0900

    Fix: FetchReposUsecase 수정

--- application/usecase/github/FetchReposUsecase.ts
+++ application/usecase/github/FetchReposUsecase.ts
@@ -5,6 +5,30 @@ export class FetchReposUsecase {
     constructor(private readonly repo: GithubRepoRepository) { }
 
     async execute(token: string): Promise<GithubRepoDto[]> {
-        return await this.repo.fetchAll(token);
+        const repos = await this.repo.fetchAll(token);
+
+        return repos.map(repo => new GithubRepoDto(
+            repo.id,
+            repo.name,
+            repo.nameWithOwner,
+            repo.url,
+            repo.isPrivate,
+            repo.description,
+            repo.updatedAt.toISOString(),
+            repo.stargazerCount,
+            repo.languageName,
+            repo.languageColor
+        ));
     }
 }
+
+// import { GithubRepoRepository } from "@/domain/repositories/GithubRepoRepository";
+// import { GithubRepoDto } from "./dto/GithubRepoDto";
+
+// export class FetchReposUsecase {
+//     constructor(private readonly repo: GithubRepoRepository) { }
+
+//     async execute(token: string): Promise<GithubRepoDto[]> {
+//         return await this.repo.fetchAll(token);
+//     }
+// }
