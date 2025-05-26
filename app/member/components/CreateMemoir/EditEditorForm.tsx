2d0bdda49b885251cc77b6ea428313909bfab775
Yoonstar <yagobo1110@naver.com>
2025-05-26 17:42:57 +0900

    Refactor: hasText 최적화

--- app/member/components/CreateMemoir/EditEditorForm.tsx
+++ app/member/components/CreateMemoir/EditEditorForm.tsx
@@ -5,7 +5,7 @@ import { EditorFormHandle } from "@/types/memoir/Memoir";
 import { Value } from "@udecode/plate";
 import { X } from "lucide-react";
 import { Session } from "next-auth";
-import { useCallback, useRef, useState } from "react";
+import { useCallback, useMemo, useRef, useState } from "react";
 import { PlateEditor } from "./plate-editor/ui/plate-editor";
 
 type EditEditorFormProps = {
@@ -57,9 +57,11 @@ export default function EditEditorForm({
     }, []);
 
     // content에 글씨가 있는지 확인하는 함수
-    const hasText = content.some((node) =>
-        node.children.some((ch: any) => ch.text?.trim() !== "")
-    );
+    const hasText = useMemo(() => {
+        return content.some((node) =>
+            node.children.some((ch: any) => ch.text?.trim() !== "")
+        );
+    }, [content]);
 
     // 폼 입력 유효성 검사 (제목, 내용)
     const formDisabled = !title.trim() || !hasText;
