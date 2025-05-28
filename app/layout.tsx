ccb3767986935a7242f624aea179e95a0c1690fc
okang1014 <okang1014@gmail.com>
2025-05-28 14:26:20 +0900

    Fix: suppressHydrationWarning 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,7 +37,7 @@ export default function RootLayout({
     children: React.ReactNode;
 }>) {
     return (
-        <html lang="en">
+        <html lang="en" suppressHydrationWarning>
             <body className="bg-bg-primary1">
                 <Providers>
                     <Header />
