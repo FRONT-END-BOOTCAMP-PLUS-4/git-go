9e4ba564e05e1d5f8ae389d21de25c7595ca72a8
Jihoon Kang <okang1014@gmail.com>
2025-05-28 14:29:56 +0900

    Feat: Metadata 추가 (#263)
    * Chore: metadata 추가
    
    * Chore: Meta 컴포넌트 추가
    
    * Fix: suppressHydrationWarning 추가
    
    * Chore: Metadata 수정

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,35 @@ import Header from "./components/Header";
 import { Providers } from "./components/Providers";
 import "./globals.css";
 
+export const metadata = {
+    title: "GITGO - 기록이 남는 개발",
+    description:
+        "개발자의 커밋과 PR 활동을 기반으로, 회고를 쉽게 작성하고 성장 스토리를 쌓아가는 서비스 — 지금 바로 시작해보세요.",
+    metadataBase: new URL("http://git-go.co.kr"),
+    openGraph: {
+        title: "GITGO - 기록이 남는 개발",
+        description:
+            "개발자의 커밋과 PR 활동을 기반으로, 회고를 쉽게 작성하고 성장 스토리를 쌓아가는 서비스 — 지금 바로 시작해보세요.",
+        url: "git-go.co.kr",
+        siteName: "GITGO",
+        images: [
+            {
+                url: "/logo.png",
+                width: 800,
+                height: 600,
+            },
+        ],
+        type: "website",
+    },
+    twitter: {
+        card: "summary_large_image",
+        title: "GITGO - 기록이 남는 개발",
+        description:
+            "개발자의 커밋과 PR 활동을 기반으로, 회고를 쉽게 작성하고 성장 스토리를 쌓아가는 서비스 — 지금 바로 시작해보세요.",
+        images: ["/logo.png"],
+    },
+};
+
 export default function RootLayout({
     children,
 }: Readonly<{
@@ -9,9 +38,6 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en" suppressHydrationWarning>
-            <head>
-                <title>GitGo - 기록이 남는 개발</title>
-            </head>
             <body className="bg-bg-primary1">
                 <Providers>
                     <Header />
