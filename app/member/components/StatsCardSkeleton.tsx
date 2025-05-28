b692780e34d5480137e075c6ba414e1a6bbe5c8f
Parkchanwoo <parkchanwoo7@naver.com>
2025-05-28 10:25:59 +0900

    Chore: 통계페이지에서 히트맵 나오는거보다 스켈레톤이 위로 올라가는 현상 수정

--- app/member/components/StatsCardSkeleton.tsx
+++ app/member/components/StatsCardSkeleton.tsx
@@ -1,6 +1,6 @@
 export default function StatsCardSkeleton() {
     return (
-        <div className="border-border-primary1 flex h-20 animate-pulse flex-col justify-center rounded-xl border bg-white p-3 shadow-sm">
+        <div className="border-border-primary1 flex h-20 flex-col justify-center rounded-xl border bg-white p-3 shadow-sm">
             <div className="mb-2 h-4 w-1/3 rounded bg-gray-200" />
             <div className="h-6 w-1/2 rounded bg-gray-300" />
         </div>
