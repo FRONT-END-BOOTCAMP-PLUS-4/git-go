575c09ab0d450214a15442ccfe910803b382f172
dongsuseo <poopoo0515@naver.com>
2025-05-29 14:23:38 +0900

    Style: 라이트모드 스켈레톤 수정

--- app/member/components/StatsCardSkeleton.tsx
+++ app/member/components/StatsCardSkeleton.tsx
@@ -1,6 +1,6 @@
 export default function StatsCardSkeleton() {
     return (
-        <div className="border-border-primary1 flex h-20 flex-col justify-center rounded-xl border p-3 shadow-sm">
+        <div className="border-border-primary1 bg-bg-member1 flex h-20 flex-col justify-center rounded-xl border p-3 shadow-sm">
             <div className="bg-bg-skeleton1 mb-2 h-4 w-1/3 rounded" />
             <div className="bg-bg-skeleton1 h-6 w-1/2 rounded" />
         </div>
