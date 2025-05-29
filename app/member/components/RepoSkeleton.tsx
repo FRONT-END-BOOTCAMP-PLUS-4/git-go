899e43f63ccb6cc71803ea123f48c3b744ead308
dongsuseo <poopoo0515@naver.com>
2025-05-29 10:39:15 +0900

    Style: 스켈레톤 다크모드 추가

--- app/member/components/RepoSkeleton.tsx
+++ app/member/components/RepoSkeleton.tsx
@@ -4,20 +4,20 @@ export default function RepoSkeleton() {
             {Array.from({ length: 5 }).map((_, i) => (
                 <li
                     key={i}
-                    className="animate-pulse border-b border-gray-200 p-4 last:border-b-0"
+                    className="border-bg-skeleton1 animate-pulse border-b p-4 last:border-b-0"
                 >
                     <div className="flex items-start justify-between">
                         <div className="flex w-full items-center gap-2">
-                            <div className="h-4 w-4 rounded bg-gray-300" />
+                            <div className="bg-bg-skeleton2 h-4 w-4 rounded" />
                             <div className="flex w-full flex-col gap-2">
-                                <div className="h-4 w-1/2 rounded bg-gray-300" />
-                                <div className="h-3 w-1/3 rounded bg-gray-200" />
+                                <div className="bg-bg-skeleton2 h-4 w-1/2 rounded" />
+                                <div className="bg-bg-skeleton1 h-3 w-1/3 rounded" />
                             </div>
                         </div>
-                        <div className="h-3 w-20 rounded bg-gray-200" />
+                        <div className="bg-bg-skeleton1 h-3 w-20 rounded" />
                     </div>
-                    <div className="mt-2 ml-6 h-3 w-3/4 rounded bg-gray-200" />
-                    <div className="mt-2 ml-6 h-2 w-24 rounded bg-gray-200" />
+                    <div className="bg-bg-skeleton1 mt-2 ml-6 h-3 w-3/4 rounded" />
+                    <div className="bg-bg-skeleton1 mt-2 ml-6 h-2 w-24 rounded" />
                 </li>
             ))}
         </ul>
--- app/member/pull-requests/components/PrCardSkeleton.tsx
+++ app/member/pull-requests/components/PrCardSkeleton.tsx
@@ -2,29 +2,29 @@ export default function PrCardSkeleton() {
     return (
         <li className="border-border-primary1 animate-pulse border-b p-4 last:border-b-0">
             <article className="flex items-start gap-x-4">
-                <div className="h-10 w-10 rounded-full bg-gray-200" />
+                <div className="bg-bg-skeleton1 h-10 w-10 rounded-full" />
 
                 <div className="flex flex-1 flex-col gap-y-3">
                     <div className="flex items-center gap-x-3">
-                        <div className="h-6 w-48 rounded bg-gray-300" />
-                        <div className="h-5 w-16 rounded bg-gray-200" />
-                        <div className="ml-auto h-5 w-20 rounded bg-gray-200" />
+                        <div className="bg-bg-skeleton2 h-6 w-48 rounded" />
+                        <div className="bg-bg-skeleton1 h-5 w-16 rounded" />
+                        <div className="bg-bg-skeleton1 ml-auto h-5 w-20 rounded" />
                     </div>
 
-                    <div className="h-4 w-100 rounded bg-gray-200" />
+                    <div className="bg-bg-skeleton1 h-4 w-100 rounded" />
 
                     <div className="flex items-center gap-x-3">
                         <div className="flex items-center gap-x-1">
-                            <div className="h-4 w-4 rounded bg-gray-200" />
-                            <div className="h-4 w-24 rounded bg-gray-200" />
+                            <div className="bg-bg-skeleton1 h-4 w-4 rounded" />
+                            <div className="bg-bg-skeleton1 h-4 w-24 rounded" />
                         </div>
 
                         <div className="flex items-center gap-x-1">
-                            <div className="h-4 w-5 rounded bg-gray-200" />
-                            <div className="h-4 w-20 rounded bg-gray-200" />
+                            <div className="bg-bg-skeleton1 h-4 w-5 rounded" />
+                            <div className="bg-bg-skeleton1 h-4 w-20 rounded" />
                         </div>
 
-                        <div className="ml-auto h-8 w-24 rounded bg-gray-200" />
+                        <div className="bg-bg-skeleton1 ml-auto h-8 w-24 rounded" />
                     </div>
                 </div>
             </article>
