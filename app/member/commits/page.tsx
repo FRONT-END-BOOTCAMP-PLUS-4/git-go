a9043c411b6094e0000b0eff7fb2098374f68061
okang1014 <okang1014@gmail.com>
2025-05-30 10:50:33 +0900

    Fix: 목록 조회 에러 수정

--- app/member/commits/page.tsx
+++ app/member/commits/page.tsx
@@ -206,7 +206,9 @@ export default function CommitPage() {
                             <CommitCardSkeleton key={index} />
                         ))
                     ) : totalCount !== 0 ? (
-                        <>{commitList}</>
+                        commitList.length > 0 ? (
+                            <>{commitList}</>
+                        ) : null
                     ) : (
                         <EmptyResult message="선택한 저장소에 표시할 커밋이 없습니다." />
                     )}
