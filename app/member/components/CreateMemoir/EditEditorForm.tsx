2401ea7814489c7e3e26747d54e1176eb1334b28
Yoonstar <yagobo1110@naver.com>
2025-06-02 10:20:11 +0900

    Style: 태그 X 버튼 shrink-0 속성 추가

--- app/member/components/CreateMemoir/EditEditorForm.tsx
+++ app/member/components/CreateMemoir/EditEditorForm.tsx
@@ -150,7 +150,7 @@ export default function EditEditorForm({
                             {tag}
                             <X
                                 onClick={() => removeTag(tag)}
-                                className="ml-1 cursor-pointer"
+                                className="ml-1 shrink-0 cursor-pointer"
                                 size={14}
                             />
                         </span>
