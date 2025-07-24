16a54a41d26dd7d85a5ecfef6420c050084b6535
okang1014 <okang1014@gmail.com>
2025-07-24 16:29:38 +0900

    Style: TimeFilter 컴포넌트 반응형 추가

--- app/member/components/TimeFilter.tsx
+++ app/member/components/TimeFilter.tsx
@@ -22,13 +22,13 @@ export default function TimeFilter({ options }: TimeFilterProps) {
     };
 
     return (
-        <div className="bg-bg-member1 border-border-primary1 rounded-md border p-4">
-            <label className="text-text-primary1 text-sm font-normal">
+        <div className="bg-bg-member1 border-border-primary1 flex items-center gap-x-2 rounded-md border p-2 md:block md:p-4">
+            <label className="text-text-primary1 shrink-0 text-xs font-normal sm:text-sm">
                 작성 기간
             </label>
             <div className="relative inline-block w-full">
                 <button
-                    className="border-border-primary1 mt-2 flex w-full cursor-pointer items-center justify-between rounded-md border px-2 py-2 text-left text-sm focus:outline-none"
+                    className="border-border-primary1 flex w-full cursor-pointer items-center justify-between rounded-md border px-2 py-2 text-left text-xs focus:outline-none sm:text-sm md:mt-2"
                     onClick={() => setIsOpen((prev) => !prev)}
                 >
                     <span>
@@ -45,7 +45,7 @@ export default function TimeFilter({ options }: TimeFilterProps) {
                     <ul className="border-border-primary1 bg-bg-member1 absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md border shadow-lg">
                         {options.map((option) => (
                             <li
-                                className="hover:bg-bg-primary1 cursor-pointer px-2 py-2 text-sm"
+                                className="hover:bg-bg-primary1 cursor-pointer px-2 py-2 text-xs sm:text-sm"
                                 key={option.value}
                                 onClick={() => handleOptionClick(option.value)}
                             >
