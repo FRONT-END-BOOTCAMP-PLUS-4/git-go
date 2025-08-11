42c866f4923b5427677f7b448d8e37bb8fcb53a6
dongsuseo <poopoo0515@naver.com>
2025-08-11 16:11:39 +0900

    Fix: panel 삭제

--- app/member/components/CreateMemoir/AccordionSideBar.tsx
+++ app/member/components/CreateMemoir/AccordionSideBar.tsx
@@ -2,7 +2,6 @@ import { ChevronsLeft, ChevronsRight } from "lucide-react";
 
 import useBuildFileTree from "@/hooks/useBuildFileTree";
 import { useState } from "react";
-import { Panel } from "react-resizable-panels";
 import FileNodeComponent from "./FileNodeComponent";
 
 type AccordionSidebarProps = {
@@ -52,29 +51,27 @@ export default function AccordionSidebar({
     }
 
     return (
-        <Panel className="flex h-full w-full">
-            <div className="bg-bg-primary1 pm-4 top-[65px] left-0 z-40 flex h-full w-full flex-col truncate pt-4 pl-4 shadow-md lg:w-[20vw]">
-                <div className="mb-4 flex items-center justify-between">
-                    <h2 className="text-sm font-bold">Changed Files</h2>
-                    <button
-                        onClick={() => setSidebarOpen(false)}
-                        className="text-text-primary1 mr-4 hidden cursor-pointer lg:block"
-                    >
-                        <ChevronsLeft />
-                    </button>
-                </div>
-
-                <div className="overflow-x-auto overflow-y-auto">
-                    {tree.map((node) => (
-                        <FileNodeComponent
-                            key={node.path}
-                            node={node}
-                            onSelect={onSelect}
-                            selectedFile={selectedFile}
-                        />
-                    ))}
-                </div>
+        <div className="bg-bg-primary1 pm-4 top-[65px] left-0 z-40 flex h-full w-full flex-col truncate pt-4 pl-4 shadow-md lg:w-[20vw]">
+            <div className="mb-4 flex items-center justify-between">
+                <h2 className="text-sm font-bold">Changed Files</h2>
+                <button
+                    onClick={() => setSidebarOpen(false)}
+                    className="text-text-primary1 mr-4 hidden cursor-pointer lg:block"
+                >
+                    <ChevronsLeft />
+                </button>
             </div>
-        </Panel>
+
+            <div className="overflow-x-auto overflow-y-auto">
+                {tree.map((node) => (
+                    <FileNodeComponent
+                        key={node.path}
+                        node={node}
+                        onSelect={onSelect}
+                        selectedFile={selectedFile}
+                    />
+                ))}
+            </div>
+        </div>
     );
 }
