a85e0f8c4aec31ed89fcc8447bdefad0d41947a9
Dongsu Seo <52390233+dongsuseo@users.noreply.github.com>
2025-08-26 10:22:50 +0900

    Feat: 회고록 모바일 UI 작업 (#408)
    * Feat: 모바일 PageTab 컴포넌트 추가
    
    * Style: 일정 해상도 이하로 내려가는 경우 PageTab 숨김 처리
    
    * Style: 반응형 대응을 위한 layout-padding 수정
    
    * Style: 반응형 대응을 위한 상단 메뉴(저장소 선택, 검색 필터) 레이아웃 조정 중
    
    * Style: 검색바 일정 해상도 도달 시 하단 마진 제거
    
    * Style: 커밋/PR 필터 일정 해상도 도달 시 레이아웃 조정
    
    * Style: 회고 페이지 상단 필터 배치 확인 작업
    
    Style: 회고 페이지 상단 필터 배치 확인 작업
    
    * Feat: 모바일 버전 저장소 선택 selectbox 컴포넌트 추가
    
    * Feat: 모바일 버전 저장소 선택 selectbox RepoSelectModal 출력 추가
    
    * Style: 모바일 페이지탭 디자인 수정
    
    * Feat: 모바일 버전 저장소 선택 selectbox 저장소 선택 기능 추가
    
    * Style: 모바일 버전 저장소 선택 저장소 hover 시 애니메이션 추가
    
    Style: 모바일 버전 저장소 선택 저장소 hover 시 애니메이션 추가
    
    Style: 모바일 버전 저장소 선택 저장소 hover 시 애니메이션 추가
    
    * Style: TimeFilter 컴포넌트 반응형 추가
    
    * Style: CommitPrFilter, TagFilter 레이아웃 및 폰트 조정
    
    Style: CommitPrFilter, TagFilter 레이아웃 및 폰트 조정
    
    * Style: TagFilter props 전달
    
    Style: TagFilter props 전달
    
    * Style: 회고록카드 모바일 UI 작업
    
    * Style: 회고록 제목 길이 모바일 기준 수정
    
    * Style: 라벨 오른쪽 끝으로 정렬
    
    * Feat: 모바일 필터 바텀시트로 구현
    
    * Fix: 필터 적용상태가 안보이는 오류 수정
    
    * Style: 커서 스타일 변경
    
    * Style: 너비 제한 해제
    
    * Style: PC버전에서 필터 버튼이 보이는 현상 수정
    
    * Style: 라벨 truncate 제거
    
    * Style: pc버전일때 회고록 카드 일정 크기이상 안줄어드는 문제 수정
    
    * Style: 타이틀이 긴 경우 컴포넌트가 넘치는 현상 수정
    
    * Style: 로딩 컴포넌트 타이틀이 비정상적으로 긴 현상 수정
    
    * Style: searchfilter 너비 repo선택 컴포넌트에 맞게 조정
    
    * Style: 일정 px에서 검색 컴포넌트 튀어나오는 현상 수정
    
    * Fix: 모바일 필터 확인 버튼 삭제
    
    ---------
    
    Co-authored-by: okang1014 <okang1014@gmail.com>

--- app/member/commits/components/CommitCardSkeleton.tsx
+++ app/member/commits/components/CommitCardSkeleton.tsx
@@ -1,15 +1,15 @@
 export function CommitCardSkeleton() {
     return (
-        <li className="border-border-primary1 animate-pulse border-b p-4">
+        <li className="border-border-primary1 my-1 animate-pulse border-b p-4">
             <article className="flex items-start gap-x-4">
                 <div className="bg-bg-skeleton1 flex h-10 w-10 items-center justify-center rounded-full" />
-                <div className="flex flex-1 flex-col gap-y-1">
+                <div className="flex min-w-0 flex-1 flex-col gap-y-2 md:gap-y-1">
                     <div className="text-text-secondary2 mb-3 flex items-center gap-x-3 text-xs">
                         <div className="bg-bg-skeleton1 h-4 w-12 rounded" />
                         <div className="bg-bg-skeleton1 h-5 w-16 rounded shadow-sm" />
                         <div className="bg-bg-skeleton1 ml-auto h-4 w-20 rounded" />
                     </div>
-                    <div className="bg-bg-skeleton2 h-5 w-100 rounded" />
+                    <div className="bg-bg-skeleton2 h-5 w-[80%] rounded" />
                     <div className="mt-2 flex items-center gap-x-3">
                         <div className="text-text-secondary2 flex items-center gap-x-1">
                             <div className="bg-bg-skeleton1 h-3 w-3 rounded" />
